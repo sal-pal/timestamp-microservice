e8193f79adfeb4d32a9836cb7df29344b00b3a60
Sal P <salvatpalvat@gmail.com>
2016-12-16 23:12:47 -0500

    Enabled CORS on server

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var express = require("express")
 var app = express()
 
 app.get("/:input", (req, res) => {
+    //Access origin header from request
+    const headerVal = req.headers.origin
+    
+    //Assign headerVal to Access-Control-Allow-Origin response header
+    res.set('Access-Control-Allow-Origin', headerVal)
+    
+    //Send response data
     var convertedData = timestampMicroServ(req.params.input)
     res.end(convertedData)
 })
